968d47f2a7325cdf7f3f1605d6d261981b6da9fd
ash4995 <yash95shah@gmail.com>
2019-09-03 11:53:07 -0700

    Got the api key and using requests to fetch

--- script.py
+++ script.py
@@ -1,4 +1,11 @@
-#Script to perform mac address lookup API
+import pprint 
+import requests
+import os
 
+API_KEY= os.getenv('API_K')
+mac_id=input("Enter the mac-id that you want to search for - (You can use octet delimiters including ':', '.' or even no delimiter)") 
 
-#dsad
+r = requests.get('https://api.macaddress.io/v1?apiKey=' + API_KEY + '&output=json&search='+mac_id)
+st = r.json()
+
+pprint.pprint (st['vendorDetails']['companyName'])
