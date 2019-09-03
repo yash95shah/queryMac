5a5b9bc037bccec244202e8fc69839a2ccb40241
ash4995 <yash95shah@gmail.com>
2019-09-03 13:24:18 -0700

    Completed the docker process

---
+++ src-file/script.py
@@ -0,0 +1,16 @@
+import pprint 
+import requests
+import os
+
+API_KEY=os.environ['API_K']
+try:
+    mac_id=input("Enter the mac-id that you want to search for -") 
+except EOFError as e:
+    print (e)
+
+try:
+    r = requests.get('https://api.macaddress.io/v1?apiKey=' + API_KEY + '&output=json&search='+mac_id)
+    st = r.json()
+    print ("The mac id belongs to: - "+ st['vendorDetails']['companyName'])
+except KeyError as k:
+    print (k)
